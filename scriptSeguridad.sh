d74a673bfc2ab8721ba415e0ed81dfbc87f89041
Landeta120 <62515158+Landeta120@users.noreply.github.com>
2022-10-06 16:28:04 +0200

    Add files via upload

---
+++ scriptSeguridad.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+printf A
+grep -o -i A a | wc -l
+printf B
+grep -o -i B a | wc -l
+printf C
+grep -o -i C a | wc -l
+printf D
+grep -o -i D a | wc -l
+printf E
+grep -o -i E a | wc -l
+printf F
+grep -o -i F a | wc -l
+printf G
+grep -o -i G a | wc -l
+printf H
+grep -o -i H a | wc -l
+printf I
+grep -o -i I a | wc -l
+printf J
+grep -o -i J a | wc -l
+printf K
+grep -o -i K a | wc -l
+printf L
+grep -o -i L a | wc -l
+printf M
+grep -o -i M a | wc -l
+printf N
+grep -o -i N a | wc -l
+printf Ñ
+grep -o -i Ñ a | wc -l
+printf O
+grep -o -i O a | wc -l
+printf P
+grep -o -i P a | wc -l
+printf Q
+grep -o -i Q a | wc -l
+printf R
+grep -o -i R a | wc -l
+printf S
+grep -o -i S a | wc -l
+printf T
+grep -o -i T a | wc -l
+printf U
+grep -o -i U a | wc -l
+printf V
+grep -o -i V a | wc -l
+printf W
+grep -o -i W a | wc -l
+printf X
+grep -o -i X a | wc -l
+printf Y
+grep -o -i Y a | wc -l
+printf Z
+grep -o -i Z a | wc -l
